ecf7347587429436a7af8c44a3410af7b1917517
Logan Franken <loganfranken@gmail.com>
2017-01-02 19:59:09 -0800

    Hooked downloading from FTP up to scraping

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
+'use strict';
+
+var config = require('../config');
+var downloadTranscripts = require('../lib/download-transcripts');
 var express = require('express');
 var router = express.Router();
+var scrapeTranscripts = require('../lib/scrape-transcripts');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Digital Monologue' });
+});
+
+router.get('/transcripts', function(req, res, next) {
+
+  downloadTranscripts(config).then(() => {
+    scrapeTranscripts(config).then(transcripts => {
+      res.json(transcripts);
+    });
+  });
+
 });
 
 module.exports = router;
